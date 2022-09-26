5b27ed76c3c7788bf4c4fe24e0684f554c2ba868
Heavenston <malolegendrelemaire@@gmail.com>
2022-09-26 13:11:52 +0200

    BUTTONS

---
+++ website/src/components/button/button.tsx
@@ -0,0 +1,13 @@
+import { component$, useStyles$, Slot, PropsOf, DOMAttributes } from "@builder.io/qwik";
+import styles from "./button.css?inline";
+
+export default component$<DOMAttributes<HTMLButtonElement>>(props => {
+    useStyles$(styles);
+    return (
+        <button {...props}>
+            <span>
+                <Slot />
+            </span>
+        </button>
+    );
+});
--- website/src/components/fax_machine/fax_machine.tsx
+++ website/src/components/fax_machine/fax_machine.tsx
@@ -2,12 +2,24 @@ import { $, component$, useRef, useOnWindow, useStyles$, useClientEffect$, useSt
 import styles from "./fax_machine.css?inline";
 
 import Display from "~/components/display/display";
+import Button from "~/components/button/button";
 
 export default component$(() => {
     useStyles$(styles);
     return (
         <div class="machine">
             <Display text="ABCDEFGHIJKLMNOPQRSTUVWXYZ l'alphabet est" />
+            <div class="lower_part">
+                <div class="keyboard">
+                    <Button>1 </Button> <Button>2 <span>ABC</span></Button> <Button>3 <span>DEF</span></Button>
+                    <Button>4 <span>GHI</span></Button> <Button>5 <span>JKL</span></Button> <Button>6 <span>MNO</span></Button>
+                    <Button>7 <span>PQRS</span></Button> <Button>8 <span>TUV</span></Button> <Button>9 <span>WXYZ</span></Button>
+                    <Button>A </Button> <Button>0 </Button> <Button>B</Button>
+                </div>
+                <div>
+                    <Button class="send-button">Send!</Button>
+                </div>
+            </div>
         </div>
     );
 });
