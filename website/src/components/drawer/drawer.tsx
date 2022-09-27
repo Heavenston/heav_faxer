410bb298eab4e074dc6ded0cebb37bee35f1ab4f
Heavenston <malolegendrelemaire@@gmail.com>
2022-09-27 22:56:01 +0200

    Nearly perfect paper exit

--- website/src/components/drawer/drawer.tsx
+++ website/src/components/drawer/drawer.tsx
@@ -1,12 +1,14 @@
-import { component$, useRef, useStylesScoped$, useClientEffect$, useStore, useWatch$ } from "@builder.io/qwik";
+import { component$, createContext, useRef, useStylesScoped$, useClientEffect$, useStore, useWatch$, useContextProvider } from "@builder.io/qwik";
 import styles from "./drawer.scss?inline";
 
 import Paper from "./paper";
 
+export const prevent_drawer_close_context = createContext<{ count: number }>("prevent_drawer_close");
+
 export function D({ label, index }: { label: string, index: number }) {
     return <button tabIndex={index}>
         <div class="background">
-            {Array(13).fill(null).map(_ => <Paper rotation={Math.random() * 1 - 0.5} move={Math.random() * 8 - 4} />)}
+            {Array(13).fill(null).map(_ => <Paper />)}
         </div>
         <div class="foreground">
             <span>{label}</span>
@@ -16,8 +18,13 @@ export function D({ label, index }: { label: string, index: number }) {
 
 export default component$(() => {
     useStylesScoped$(styles);
+    const prevent_drawer_close = useStore({
+        count: 0,
+    });
+    useContextProvider(prevent_drawer_close_context, prevent_drawer_close);
+    console.log(prevent_drawer_close.count);
 
-    return <div class="drawer">
+    return <div class={`drawer ${prevent_drawer_close.count > 0 ? "prevent-close" : ""}`}>
         <D label="Files" index={2} />
         <D label="Links" index={1} />
     </div>;
--- website/src/components/drawer/paper.tsx
+++ website/src/components/drawer/paper.tsx
@@ -1,13 +1,66 @@
-import { component$, useRef, useStylesScoped$, useClientEffect$, useStore, useWatch$ } from "@builder.io/qwik";
+import { component$, useRef, useStyles$, useStylesScoped$, useClientEffect$, useStore, useWatch$, useContext } from "@builder.io/qwik";
 import styles from "./paper.scss?inline";
+import globalStyles from "./paper_global.scss?inline";
 
-export default component$<{ rotation: number, move: number }>(({ rotation, move }) => {
+import { prevent_drawer_close_context  } from "./drawer";
+
+export function trunc(x: number): number {
+    return Math.trunc(x * 10000) / 10000;
+}
+
+export default component$(() => {
     useStylesScoped$(styles);
+    useStyles$(globalStyles);
 
-    rotation = Math.trunc(rotation * 10000) / 10000;
-    move = Math.trunc(move * 10000) / 10000;
+    const state = useStore({
+        opened: false,
+        rotation: Math.random() * 1 - 0.5,
+        move: Math.random() * 8 - 4,
+    });
+    const ref = useRef<HTMLDivElement>();
+
+    const prevent_drawer_close = useContext(prevent_drawer_close_context);
+    
+    useClientEffect$(({ track }) => {
+        const element = track(ref, "current");
+        if (element === undefined) return;
+
+        const onMouseEnter = () => { state.opened = true; };
+        const onMouseLeave = () => { state.opened = false; };
+        element.addEventListener("mouseenter", onMouseEnter);
+        element.addEventListener("mouseleave", onMouseLeave);
+
+        return () => { element.removeEventListener("mouseenter", onMouseEnter); element.removeEventListener("mouseleave", onMouseLeave); };
+    });
+
+    useClientEffect$(({ track }) => {
+        const el = track(ref, "current");
+        const opened = track(state, "opened");
+        if (!opened || el === undefined) return;
+
+        const bcr = el.getBoundingClientRect();
+        const floating_paper = document.createElement("div");
+        floating_paper.className = "floating-paper";
+        document.body.appendChild(floating_paper);
+        floating_paper.style.left = `${bcr.left}px`;
+        floating_paper.style.top = `${bcr.top}px`;
+        floating_paper.style.width = `${bcr.width}px`;
+        floating_paper.style.setProperty("--rot", el.style.getPropertyValue("--rot"))
+
+        prevent_drawer_close.count += 1;
+
+        return () => {
+            floating_paper.classList.add("hide");
+            setTimeout(() => {
+                document.body.removeChild(floating_paper);
+                prevent_drawer_close.count -= 1;
+            }, 300);
+        };
+    });
+
     return <div
+        ref={ref}
         class="paper"
-        style={`--rot: ${rotation}deg; --move: ${move}px`}
+        style={ `--rot: ${trunc(state.rotation)}deg; --move: ${trunc(state.move)}px;` }
     />
 });
