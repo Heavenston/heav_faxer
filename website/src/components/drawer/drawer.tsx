232b988c9f9b9a4a8227a2d6fb427a23ff70a0dc
Heavenston <malolegendrelemaire@@gmail.com>
2022-09-27 20:36:45 +0200

    Separated into lite component

--- website/src/components/drawer/drawer.tsx
+++ website/src/components/drawer/drawer.tsx
@@ -1,21 +1,21 @@
 import { component$, useRef, useStyles$, useClientEffect$, useStore, useWatch$ } from "@builder.io/qwik";
 import styles from "./drawer.scss?inline";
 
+export function D({ label, index }: { label: string, index: number }) {
+    return <button tabIndex={index}>
+        <div class="background">
+        </div>
+        <div class="foreground">
+            <span>{label}</span>
+        </div>
+    </button>;
+}
+
 export default component$(() => {
     useStyles$(styles);
 
     return <div class="drawer">
-        <button tabIndex={2} style="--label: 'Files';">
-            <div class="background" />
-            <div class="foreground">
-                <span>Files</span>
-            </div>
-        </button>
-        <button tabIndex={1}>
-            <div class="background" />
-            <div class="foreground">
-                <span>Links</span>
-            </div>
-        </button>
+        <D label="Files" index={2} />
+        <D label="Links" index={1} />
     </div>;
 });
