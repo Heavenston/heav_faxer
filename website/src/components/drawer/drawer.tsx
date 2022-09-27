475c1417b74502d3ceee926b9f2c9d6925086190
Heavenston <malolegendrelemaire@@gmail.com>
2022-09-27 21:29:04 +0200

    Mov'em

--- website/src/components/drawer/drawer.tsx
+++ website/src/components/drawer/drawer.tsx
@@ -1,14 +1,16 @@
 import { component$, useRef, useStylesScoped$, useClientEffect$, useStore, useWatch$ } from "@builder.io/qwik";
 import styles from "./drawer.scss?inline";
 
-export function P(){
-    return <div />
+export function P({ rotation, move }: { rotation: number, move: number }){
+    rotation = Math.trunc(rotation * 10000) / 10000;
+    move = Math.trunc(move * 10000) / 10000;
+    return <div style={`--rot: ${rotation}deg; --move: ${move}px`} />
 }
 
 export function D({ label, index }: { label: string, index: number }) {
     return <button tabIndex={index}>
         <div class="background">
-            {Array(13).fill(null).map(_ => <P />)}
+            {Array(13).fill(null).map(_ => <P rotation={Math.random() * 1 - 0.5} move={Math.random() * 8 - 4} />)}
         </div>
         <div class="foreground">
             <span>{label}</span>
