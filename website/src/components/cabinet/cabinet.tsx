995b8d6e08e02c6772680fc84178f692c0123b4e
Heavenston <malolegendrelemaire@@gmail.com>
2022-09-27 23:27:11 +0200

    Prevent opening cabinet when other cabinet has papers

--- website/src/components/cabinet/cabinet.tsx
+++ website/src/components/cabinet/cabinet.tsx
@@ -1,10 +1,12 @@
 import { component$, createContext, useRef, useStylesScoped$, useClientEffect$, useStore, useWatch$, useContextProvider } from "@builder.io/qwik";
 import styles from "./cabinet.scss?inline";
 
-import Drawer from "./drawer";
+import { default as Drawer, prevent_drawer_open_context } from "./drawer";
 
 export default component$(() => {
     useStylesScoped$(styles);
+    const prevent_drawer_open = useStore({ index: -1 });
+    useContextProvider(prevent_drawer_open_context, prevent_drawer_open);
 
     return <div class="cabinet">
         <Drawer label="Files" index={2} />
--- website/src/components/cabinet/drawer.tsx
+++ website/src/components/cabinet/drawer.tsx
@@ -3,6 +3,7 @@ import styles from "./drawer.scss?inline";
 
 import Paper from "./paper";
 
+export const prevent_drawer_open_context = createContext<{ index: number }>("prevent_drawer_open_context");
 export const prevent_drawer_close_context = createContext<{ count: number }>("prevent_drawer_close_context");
 
 export default component$<{ index: number, label: string }>(({ index, label }) => {
@@ -10,7 +11,21 @@ export default component$<{ index: number, label: string }>(({ index, label }) =
     const prevent_drawer_close = useStore({ count: 0 });
     useContextProvider(prevent_drawer_close_context, prevent_drawer_close);
 
-    return <button class={`drawer ${prevent_drawer_close.count > 0 ? "prevent-close" : ""}`} tabIndex={index}>
+    const prevent_drawer_open = useContext(prevent_drawer_open_context);
+    useWatch$(({ track }) => {
+        const drc = track(prevent_drawer_close, "count");
+        const dro = track(prevent_drawer_open, "index");
+        if (dro === -1 && drc > 0) {
+            prevent_drawer_open.index = index;
+        }
+        else if (prevent_drawer_open.index === index && drc == 0) {
+            prevent_drawer_open.index = -1;
+        }
+    });
+
+    const prevent_open = prevent_drawer_open.index !== -1 && prevent_drawer_open.index !== index;
+
+    return <button class={`drawer ${prevent_drawer_close.count > 0 ? "prevent-close" : ""} ${prevent_open ? "prevent-open" : ""}`} tabIndex={index}>
         <div class="background">
             {Array(13).fill(null).map(_ => <Paper />)}
         </div>
