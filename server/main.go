4adafe262c787734ddd4161cc2ff4c6c875301e8
Heavenston <malolegendrelemaire@@gmail.com>
2022-10-05 13:34:57 +0200

    Add cors headers in api directly

--- server/main.go
+++ server/main.go
@@ -9,6 +9,12 @@ import (
     "fr.faxer/apiserver/db"
 )
 
+func cors_middleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Add("Access-Control-Allow-Origin", "*")
+        next.ServeHTTP(w, r)
+    })
+}
 
 func main() {
     err := db.Connect()
@@ -18,7 +24,7 @@ func main() {
     defer db.Disonnect()
     fmt.Println("Succesfully connected to database")
 
-    http.HandleFunc("/link/", links.LinkHandlr)
-    http.HandleFunc("/api/link/", links.LinkHandlr)
+    http.Handle("/link/", cors_middleware(http.HandlerFunc(links.LinkHandlr)))
+    http.Handle("/api/link/", cors_middleware(http.HandlerFunc(links.LinkHandlr)))
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
