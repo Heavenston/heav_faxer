c8834034fb2948ef0985e9a7093f5c5d8a17c768
Heavenston <malolegendrelemaire@@gmail.com>
2022-10-05 13:39:17 +0200

    Ignore prefligt requests

--- server/main.go
+++ server/main.go
@@ -12,6 +12,10 @@ import (
 func cors_middleware(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         w.Header().Add("Access-Control-Allow-Origin", "*")
+        if r.Method == "OPTIONS" { // ignore preflight
+            return
+        }
+
         next.ServeHTTP(w, r)
     })
 }
