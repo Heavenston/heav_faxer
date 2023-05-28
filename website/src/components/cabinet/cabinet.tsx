74c7e4ac40032c2f2737d58d0385e489e9e85742
Heavenston <malolegendrelemaire@@gmail.com>
2023-05-28 16:24:12 +0100

    Removed link input paper

--- website/src/components/cabinet/cabinet.tsx
+++ website/src/components/cabinet/cabinet.tsx
@@ -1,28 +1,22 @@
 import {
     component$,
-    $,
     useStylesScoped$,
     useStore,
     useContextProvider,
-    useContext,
 } from "@builder.io/qwik";
 import styles from "./cabinet.scss?inline";
 
 import { default as Drawer, prevent_drawer_open_context } from "./drawer";
-import { link_modal_context } from "~/components/global_link_modal/global_link_modal";
 
 export default component$(() => {
     useStylesScoped$(styles);
     const prevent_drawer_open = useStore({ index: -1 });
     useContextProvider(prevent_drawer_open_context, prevent_drawer_open);
 
-    const lm = useContext(link_modal_context);
-    const link_click = $(() => { lm.enable = true; });
-
     return (
         <div class="cabinet">
             <Drawer label="Files" index={2} />
-            <Drawer label="Links" index={1} click_cb={link_click} />
+            <Drawer label="Links" index={1} />
         </div>
     );
 });
--- website/src/routes/layout.tsx
+++ website/src/routes/layout.tsx
@@ -1,22 +1,12 @@
 import {
     component$,
     Slot,
-    useContextProvider,
-    useStore,
 } from "@builder.io/qwik";
-import {
-    default as GlobalModal,
-    link_modal_context,
-} from "~/components/global_link_modal/global_link_modal";
 
 export default component$(() => {
-    const mc = useStore({ enable: false });
-    useContextProvider(link_modal_context, mc);
-
     return (
         <>
             <Slot />
-            <GlobalModal />
         </>
     );
 });
