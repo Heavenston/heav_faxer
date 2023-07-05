28e5263ee142546e69ceb85c6eb47c7e18eee82a
Heavenston <malolegendrelemaire@@gmail.com>
2023-07-05 16:04:01 +0100

    Added meta tags for cool embeds

--- website/src/root.tsx
+++ website/src/root.tsx
@@ -19,6 +19,18 @@ export default component$(() => {
         <QwikCityProvider>
             <head>
                 <meta charSet="utf-8" />
+                <meta name="title" content="Heav Faxer - Link and file shortening" />
+                <meta name="description" content="Create short versions of your links or upload files all for free." />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://faxer.heav.fr" />
+                <meta property="og:title" content="Heav Faxer - Link and file shortening" />
+                <meta property="og:description" content="Create short versions of your links or upload files all for free." />
+
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://faxer.heav.fr" />
+                <meta property="twitter:title" content="Heav Faxer - Link and file shortening" />
+                <meta property="twitter:description" content="Create short versions of your links or upload files all for free." />
+
                 <link rel="icon" type="image/png" href="/faxer_logo.png" />
                 <RouterHead />
             </head>
