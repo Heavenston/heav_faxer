3aea53789854c29d2706ace23bde64e6f0bc080e
Heavenston <malolegendrelemaire@@gmail.com>
2023-07-07 07:17:07 +0100

    Use x-forwarded-for

--- server/src/utils.rs
+++ server/src/utils.rs
@@ -9,6 +9,15 @@ impl<'r> FromRequest<'r> for RealIp {
     type Error = ();
 
     async fn from_request(req: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
-        request::Outcome::Success(RealIp(req.client_ip()))
+        if let Some(h) = req.headers().get("X-Forwarded-For").next() {
+            h.split(',').next().and_then(|x| x.parse::<IpAddr>().ok())
+                .map(|x| request::Outcome::Success(RealIp(Some(
+                    x
+                ))))
+                .unwrap_or(request::Outcome::Success(RealIp(req.client_ip())))
+        }
+        else {
+            request::Outcome::Success(RealIp(req.client_ip()))
+        }
     }
 }
