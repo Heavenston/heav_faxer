f60342a7a0af5fa039b5ea7efda629dd7f3fd777
Heavenston <malolegendrelemaire@@gmail.com>
2023-07-21 09:51:07 +0200

    Hash file progressively

--- website/src/routes/file/index.tsx
+++ website/src/routes/file/index.tsx
@@ -4,6 +4,7 @@ import { DocumentHead } from "@builder.io/qwik-city";
 import BackButton from "~/components/back_button/back_button"
 import Machine, { SendFunction } from "~/components/fax_machine/fax_machine";
 import * as api from "~/api";
+import CryptoJS from "crypto-js";
 
 import styles from "./index.scss?inline";
 
@@ -34,23 +35,50 @@ export default component$(() => {
             throw "Unexpected error, try refreshing";
         if (file_value.value === undefined)
             throw "No paper detected";
+        set_status(0, "Reading file");
         const file = file_input.value.files?.item(0);
         if (!(file instanceof File))
             throw "Expected file";
+        if (file.size > 1_000_000_000)
+            throw "File is too big";
 
         set_status(0, "Computing file hash...");
 
-        const file_content = new Uint8Array(await file.arrayBuffer());
         const file_type = file.type;
+        const file_size = file.size;
 
-        console.log(file_content.length);
+        const hasher = CryptoJS.algo.MD5.create();
+        {
+            const stream = file.stream();
+            const reader = stream.getReader();
 
-        const { default: md5 } = await import("md5");
-        const hash = btoa(String.fromCharCode.apply(null, md5(
-            file_content, { asBytes: true }
-        )));
+            let result: ReadableStreamReadResult<Uint8Array>;
+            let processed = 0;
+            while ((result = await reader.read()).value !== undefined) {
+                let word_array = CryptoJS.lib.WordArray.create(
+                    [], 0
+                );
+                for (let i = 0; i < result.value.length; i++) {
+                    const byte = result.value.at(i) ?? 0;
+                    const word_byte_idx = word_array.sigBytes % 4;
+                    if (word_byte_idx === 0)
+                        word_array.words.push(0);
+                    const last_index = word_array.words.length - 1;
+                    word_array.words[last_index] |= byte << (8 * (3 - word_byte_idx));
+                    word_array.sigBytes += 1;
+                }
+                processed += word_array.sigBytes;
 
-        console.log(file_content.length);
+                console.log(word_array);
+                hasher.update(word_array);
+
+                set_status(0, `Hashing ${Math.floor(processed / file_size * 1000) / 10}%`);
+                await new Promise<void>(r => setTimeout(r));
+            }
+        };
+        const hash = CryptoJS.enc.Base64.stringify(hasher.finalize());
+
+        console.log(file.name, "of size", file.size, "and hash", hash);
 
         set_status(0, "Requesting upload config");
 
@@ -60,7 +88,7 @@ export default component$(() => {
         const rs = await api.upload_file(
             "random",
             file_ext, file_type,
-            hash, file_content.length,
+            hash, file_size,
         );
         if (!rs.success)
             throw rs.message ?? rs.error;
@@ -69,9 +97,10 @@ export default component$(() => {
         if (rs.result === "already_known")
             return `${api.FILES_BASE_URL}${rs.name}`;
 
-        const rs2 = await (await import("axios")).default.put(
-            rs.upload_url, file_content,
-            {
+        const rs2 = await (await import("axios")).default(
+            rs.upload_url, {
+                method: "PUT",
+                data: file,
                 headers: {
                     "Content-Type": file_type,
                     "Content-md5": hash,
