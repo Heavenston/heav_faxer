a43ecf44f8bbd3077f456b8d0b50098f371b6569
Malo Legendre-Lemaire <malolegendrelemaire@gmail.com>
2026-02-28 22:59:32 +0100

    attempt: use @mjackson/multipart-parser

--- website/src/routes/api/tabs/[id]/files/[file_id]/+server.ts
+++ website/src/routes/api/tabs/[id]/files/[file_id]/+server.ts
@@ -1,39 +1,16 @@
-import { json, error, type RequestHandler } from "@sveltejs/kit";
-import { type } from "arktype";
+import { json, type RequestHandler } from "@sveltejs/kit";
+import { parseMultipartRequest } from "@mjackson/multipart-parser";
 
-const PutFileRequest = type({
-  files: type({
-    "local_id?": "string",
-    name: "string >= 1",
-    "mime_type?": "string",
-    size_bytes: "number.integer >= 0",
-  }).array().moreThanLength(0).atMostLength(100),
-});
-export type PutFileRequest = typeof PutFileRequest.infer;
+export type PutFileResponse = { };
 
-export type PutFileResponse = {
-};
+export const PUT: RequestHandler = async ({ request }) => {
+  console.log("PUTPUTPUT1");
+  const formData = parseMultipartRequest(request, { maxFileSize: 10 * 1024 * 1024 * 1024 });
+  console.log("PUTPUTPUT2");
 
-export const PUT: RequestHandler = async ({ locals, request }) => {
-  const user = locals.user;
-  if (user == null)
-    error(401);
-
-  const formData = await request.formData();
-  const file = formData.get("file");
-  if (!(file instanceof File))
-    error(400, "Invalid file given");
-  const stream = file.stream();
-  const reader = stream.getReader();
-
-  let done = false;
-  while (!done) {
-    const result = await reader.read();
-    done = result.done;
-
-    if (!result.value) continue;
-    const buffer = result.value;
-    console.log(buffer.byteLength);
+  for await (const data of formData) {
+    console.log("PUTPUTPUT2");
+    console.log(data.size);
   }
 
   return json({ } satisfies PutFileResponse);
