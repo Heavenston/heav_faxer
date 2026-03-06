ddb95239736fd99dc2eb4545ef4801d22d4b42e6
Malo Legendre-Lemaire <malolegendrelemaire@gmail.com>
2026-03-06 16:04:15 +0100

    wip: Customized qwik template

--- website/src/root.tsx
+++ website/src/root.tsx
@@ -2,7 +2,7 @@ import { component$, isDev } from "@builder.io/qwik";
 import { QwikCityProvider, RouterOutlet } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/router-head/router-head";
 
-import "./global.css";
+import "./global.scss";
 
 export default component$(() => {
   /**
--- website/vite.config.ts
+++ website/vite.config.ts
@@ -21,7 +21,14 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
  */
 export default defineConfig(({ command, mode }): UserConfig => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths({ root: "." })],
+    plugins: [qwikCity({
+      trailingSlash: false,
+    }), qwikVite({
+      devTools: {
+        clickToSource: false,
+        imageDevTools: false,
+      },
+    }), tsconfigPaths({ root: "." })],
     // This tells Vite which dependencies to pre-build in dev mode.
     optimizeDeps: {
       // Put problematic deps that break bundling here, mostly those with binaries.
@@ -32,21 +39,23 @@ export default defineConfig(({ command, mode }): UserConfig => {
     /**
      * This is an advanced setting. It improves the bundling of your server code. To use it, make sure you understand when your consumed packages are dependencies or dev dependencies. (otherwise things will break in production)
      */
-    // ssr:
-    //   command === "build" && mode === "production"
-    //     ? {
-    //         // All dev dependencies should be bundled in the server build
-    //         noExternal: Object.keys(devDependencies),
-    //         // Anything marked as a dependency will not be bundled
-    //         // These should only be production binary deps (including deps of deps), CLI deps, and their module graph
-    //         // If a dep-of-dep needs to be external, add it here
-    //         // For example, if something uses `bcrypt` but you don't have it as a dep, you can write
-    //         // external: [...Object.keys(dependencies), 'bcrypt']
-    //         external: Object.keys(dependencies),
-    //       }
-    //     : undefined,
+    ssr:
+      command === "build" && mode === "production"
+        ? {
+            // All dev dependencies should be bundled in the server build
+            noExternal: Object.keys(devDependencies),
+            // Anything marked as a dependency will not be bundled
+            // These should only be production binary deps (including deps of deps), CLI deps, and their module graph
+            // If a dep-of-dep needs to be external, add it here
+            // For example, if something uses `bcrypt` but you don't have it as a dep, you can write
+            // external: [...Object.keys(dependencies), 'bcrypt']
+            external: Object.keys(dependencies),
+          }
+        : undefined,
 
     server: {
+      port: 1234,
+
       headers: {
         // Don't cache the server response in dev mode
         "Cache-Control": "public, max-age=0",
